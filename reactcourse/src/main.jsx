1768e5cdeafbcc72d9e0efbc5f4aa97738c70f54
Thirumalainambi12 <kthirumalainambi39@gmail.com>
2025-11-22 10:54:01 -0800

    Overthetop

---
+++ reactcourse/src/main.jsx
@@ -0,0 +1,4 @@
+import { createRoot } from 'react-dom/client'
+import App from "./animepro/App.jsx";
+import { BrowserRouter} from 'react-router-dom';
+createRoot(document.getElementById('root')).render(<BrowserRouter><App /></BrowserRouter>)
